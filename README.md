d277563e1c06ac660ca9b38b27917da6b12528f6
12332199 <girikuldeep754@gmail.com>
2022-02-02 22:30:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ovgx3c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ovgx3c)
---
+++ src/Data.js
@@ -0,0 +1,9 @@
+function Data() {
+  return (
+    <div className="App">
+      dir
+      <h1>hello</h1>
+    </div>
+  );
+}
+export default Data;
